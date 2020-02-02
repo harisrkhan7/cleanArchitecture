e1ce8e96230247d1dbcafd933ac34d805439e6f3
Unknown <harisk@student.unimelb.edu.au>
2020-02-02 15:06:43 +1100

    Application Core with Product Aggregate and IAsyncRepository

---
+++ cleanArchitecture.Core/Entities/ProductAggregate/ProductOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace cleanArchitecture.Core.Entities.ProductAggregate
+{
+    public class ProductOptions :BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsNew { get; }
+
+        public Guid? ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public ProductOptions()
+        {
+        }
+    }
+}
---
+++ cleanArchitecture.Core/Interfaces/Repositories/IAsyncRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using cleanArchitecture.Core.Entities;
+
+namespace cleanArchitecture.Core.Interfaces.Repositories
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();        
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);        
+    }
+}
